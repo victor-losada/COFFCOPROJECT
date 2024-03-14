8f3be65561ba772ca8207b13c76db793103f3a2c
Lumaryy <luisamariasancheztovar2006@gmail.com>
2024-03-13 21:14:38 -0500

    crear proyecto

---
+++ Backend/Node/app.js
@@ -0,0 +1,11 @@
+import  Express  from "express";
+import bodyParser from "body-parser";
+
+const servidor = Express()
+servidor.use(bodyParser.json())
+servidor.use(bodyParser.urlencoded({extended:true}))
+
+
+servidor.listen(3000,()=>{
+console.log("servidor escuchando desde el puerto 3000")
+})
