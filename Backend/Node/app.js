ddc4e2bc1100e4d9d4eaf3a91b6674d2d0c99dc2
SS-victoria <stiven-cool@hotmail.com>
2024-03-15 16:07:46 -0500

    crud usuarios

--- Backend/Node/app.js
+++ Backend/Node/app.js
@@ -1,9 +1,13 @@
 import  Express  from "express";
 import bodyParser from "body-parser";
+import rutaUsuario from "./src/routes/usuarioRoute.js";
 
 const servidor = Express()
 servidor.use(bodyParser.json())
 servidor.use(bodyParser.urlencoded({extended:true}))
+
+
+servidor.use('/usuario',rutaUsuario)
 
 
 servidor.listen(3000,()=>{
