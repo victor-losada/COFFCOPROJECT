5d7e19a856187139d4f2d1a81a4d796aa7a74c67
SS-victoria <stiven-cool@hotmail.com>
2024-03-15 17:24:24 -0500

    crud usuarios

--- Backend/Node/app.js
+++ Backend/Node/app.js
@@ -1,10 +1,12 @@
 import  Express  from "express";
 import bodyParser from "body-parser";
+import rutaUsuario from './src/routes/usuarioRoute.js'
 
 const servidor = Express()
 servidor.use(bodyParser.json())
 servidor.use(bodyParser.urlencoded({extended:true}))
 
+servidor.use('/usuario',rutaUsuario)
 
 servidor.listen(3000,()=>{
 console.log("servidor escuchando desde el puerto 3000")
