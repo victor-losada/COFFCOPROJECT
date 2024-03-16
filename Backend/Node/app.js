760d732d87fb209c7bd19d071cb0b8ce05592c6b
Lumaryy <luisamariasancheztovar2006@gmail.com>
2024-03-15 19:31:14 -0500

    correccion

--- Backend/Node/app.js
+++ Backend/Node/app.js
@@ -1,13 +1,9 @@
 import  Express  from "express";
 import bodyParser from "body-parser";
-import rutaUsuario from "./src/routes/usuarioRoute.js";
 
 const servidor = Express()
 servidor.use(bodyParser.json())
 servidor.use(bodyParser.urlencoded({extended:true}))
-
-
-servidor.use('/usuario',rutaUsuario)
 
 
 servidor.listen(3000,()=>{
