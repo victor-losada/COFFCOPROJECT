326dd7cc43a5d2bcb4880eb56f7e3ca2fbafe7ec
Lumaryy <luisamariasancheztovar2006@gmail.com>
2024-03-16 21:20:59 -0500

    crud municipio

--- Backend/Node/app.js
+++ Backend/Node/app.js
@@ -1,9 +1,12 @@
 import  Express  from "express";
 import bodyParser from "body-parser";
+import  rutaMunicipio from "./src/routes/municipioRoute.js";
 
 const servidor = Express()
 servidor.use(bodyParser.json())
 servidor.use(bodyParser.urlencoded({extended:true}))
+
+servidor.use("/municipio",rutaMunicipio)
 
 
 servidor.listen(3000,()=>{
