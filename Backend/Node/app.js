7eb37f293325db940a9f158ac9b06b40bc6bee10
daniel22alejandro <danniel10yara@gmail.com>
2024-03-19 08:35:18 -0500

    holanda

--- Backend/Node/app.js
+++ Backend/Node/app.js
@@ -4,13 +4,19 @@ import ArchivosRoute from "./src/routes/ArchivosRoute.js"
 import FormatoRoute from './src/routes/FormatoRoute.js'
 
 
-const servidor = express()
+import  Express  from "express";
+import bodyParser from "body-parser";
+import rutaMunicipio from "./src/routes/municipioRoute.js";
+import ruta from "./src/routes/muestraRoutes.js";
+
+const servidor = Express()
 servidor.use(bodyParser.json())
-servidor.use(bodyParser.urlencoded({extended: false}))
+servidor.use(bodyParser.urlencoded({extended:true}))
 
-servidor.use("/archivo", ArchivosRoute)
-servidor.use("/formato", FormatoRoute)
+servidor.use("/municipio",rutaMunicipio)
+servidor.use("/muestra", ruta )
 
-servidor.listen(3000, () => {
-    console.log('listening on port 3000')
+
+servidor.listen(3000,()=>{
+console.log("servidor escuchando desde el puerto 3000")
 })
