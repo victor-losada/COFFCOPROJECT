d2cffb4572515fb177d805443f49d36d4035fd26
miangel07 <miguelosoriorojas063@gmail.com>
2024-03-27 14:35:06 -0500

    agragar documentacion

--- Backend/Node/app.js
+++ Backend/Node/app.js
@@ -6,11 +6,19 @@ import ArchivosRoute from "./src/routes/ArchivosRoute.js"
 import FormatoRoute from './src/routes/FormatoRoute.js'
 import rutaUsuario from './src/routes/usuarioRoute.js'
 import rutaDetalle from "./src/routes/detalleRoute.js";
+import cors from "cors"
+
 
 const servidor = Express()
 servidor.use(bodyParser.json())
 servidor.use(bodyParser.urlencoded({extended:true}))
-
+servidor.set('view engine','ejs')
+servidor.set('views','./views')
+servidor.use(Express.static('./public'))
+servidor.use(cors())
+servidor.use('/documents',(req,res)=>{
+    res.render('documentacion.ejs')
+})
 servidor.use("/municipio",rutaMunicipio)
 servidor.use("/muestra", ruta )
 servidor.use("/archivo",ArchivosRoute)
@@ -20,5 +28,5 @@ servidor.use('/detalle', rutaDetalle)
 
 
 servidor.listen(3000,()=>{
-console.log("servidor escuchando desde el puerto 3000")
+console.log("servidor escuchando desde el puerto 3000")
 })
