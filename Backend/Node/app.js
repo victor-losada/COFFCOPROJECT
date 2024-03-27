a1cd3ddd621a2c5ec94c62aa78339e22ae3c89b9
victor-losada <victorlosada2211@gmail.com>
2024-03-26 21:07:00 -0500

    New files2

--- Backend/Node/app.js
+++ Backend/Node/app.js
@@ -5,7 +5,6 @@ import ruta from "./src/routes/muestraRoutes.js";
 import ArchivosRoute from "./src/routes/ArchivosRoute.js"
 import FormatoRoute from './src/routes/FormatoRoute.js'
 import rutaUsuario from './src/routes/usuarioRoute.js'
-import Datos from './src/routes/DatosRouters.js'
 const servidor = Express()
 servidor.use(bodyParser.json())
 servidor.use(bodyParser.urlencoded({extended:true}))
@@ -15,7 +14,6 @@ servidor.use("/muestra", ruta )
 servidor.use("/archivo",ArchivosRoute)
 servidor.use("/formato",FormatoRoute)
 servidor.use('/usuario',rutaUsuario)
-servidor.use('/datos', Datos)
 
 
 servidor.listen(3000,()=>{
